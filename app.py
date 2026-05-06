956323a7e03461a6916bbd5b83b6429b9eb22007
NadaAljohani <smrkknr62@gmail.com>
2026-05-06 11:59:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def ai_is_in_scope(user_text):
 
     try:
 
-        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}"
+        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={API_KEY}"
 
         headers = {
             "Content-Type": "application/json"
@@ -241,7 +241,7 @@ def ask_ai(word, dialect):
     try:
         prompt = get_ai_persona_prompt(word, dialect)
 
-        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}"
+        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={API_KEY}"
 
         headers = {
             "Content-Type": "application/json"
