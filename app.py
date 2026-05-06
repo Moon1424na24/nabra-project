89b2166e3d29a82461603a2bc02e4172152f0608
NadaAljohani <smrkknr62@gmail.com>
2026-05-06 11:55:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
 # ----------------------------------------------------
 # تنظيف النص
 # ----------------------------------------------------
